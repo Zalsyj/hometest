1ace8d60f665f71e4dfef4fd00aa7cd997c62d64
Jérôme <Jrme@Syj>
2016-06-07 22:23:10 +0200

    Initialisation

---
+++ hometest/src/main/java/org/hometest/hometest/App.java
@@ -0,0 +1,13 @@
+package org.hometest.hometest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
