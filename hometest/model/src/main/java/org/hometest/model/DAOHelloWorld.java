6ad584f688404a165f3771ebecc48ff0c42647a2
Jérôme <Jrme@Syj>
2016-06-08 23:01:11 +0200

    - Suppression de tous les warnings de versions dupliqué du parent. - Correction d'une référence JUnit 3. - Compilation en Java 8 - Réecriture des try {} final {} des closeable , try() {} de Java 8

--- hometest/model/src/main/java/org/hometest/model/DAOHelloWorld.java
+++ hometest/model/src/main/java/org/hometest/model/DAOHelloWorld.java
@@ -32,35 +32,21 @@ public class DAOHelloWorld {
 	}
 	
 	public String getQuerySelectFirstHelloWorld() {
-		String s;		
-		try {
-			// On ouvre la connexion à la base de données
-			Connection connection = DriverManager.getConnection(url, login, password);
-			try {
-				// On crée un statement.
-				Statement st = (Statement) connection.createStatement();
-				try {
-					// On éxecute la requête.
-					ResultSet rs = (ResultSet) st.executeQuery("select * from test");
-					try {
-						if (rs.next()) {
-							s = rs.getString(1);
-						} else {
-							System.err.println("Il n'y a pas de ligne dans la table 'test'");
-							s = null;
-						}
-					} finally {
-						// On ferme la requête , même si il y a une erreur.
-						rs.close();
-					}
-				} finally {
-					// On ferme le statement, même si il y a une erreur.
-					st.close();
-				}
-			} finally {
-				// On ferme la connexion
-				connection.close();
-			}
+		String s;
+		// On ouvre la connexion à la base de données
+		try(Connection connection = DriverManager.getConnection(url, login, password)) {
+			// On crée un statement.
+			try(Statement st = (Statement) connection.createStatement()) {
+				// On éxecute la requête.
+				try(ResultSet rs = (ResultSet) st.executeQuery("select * from test")) {										
+					if (rs.next()) {
+						s = rs.getString(1);
+					} else {
+						System.err.println("Il n'y a pas de ligne dans la table 'test'");
+						s = null;
+					}					
+				} 
+			} 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			s = null;
